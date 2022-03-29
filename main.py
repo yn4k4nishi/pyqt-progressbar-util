609e8813e8f459fd2573ed5d4ac31ef7698d6131
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2022-03-29 23:41:33 +0900

    :sparkles: Feature : progress bar example

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import sys
+import time
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from ui.test import Ui_MainWindow
+from progress_bar_util import ProgressBarUtil
 
 
 class MainWindow(QMainWindow):
+    is_stopped = False
+
     def __init__(self) -> None:
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.show()
+
+        self.ui.pushButtonStart.clicked.connect(self.process)
+    
+    def process(self):
+        self.ui.pushButtonStart.setDisabled(True)
+        
+        for p in ProgressBarUtil(range(7), self.ui.progressBar):
+            time.sleep(1)
+
+        self.ui.pushButtonStart.setEnabled(True)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -17,3 +33,5 @@ if __name__ == "__main__":
     w = MainWindow()
 
     sys.exit(app.exec_())
+
+    
---
+++ progress_bar_util.py
@@ -0,0 +1,23 @@
+
+class ProgressBarUtil(object):
+    def __init__(self, list, progress_bar) -> None:
+        self.progress_bar = progress_bar
+
+        self.iter = iter(list)
+        self.max = len(list)
+        self.val = 0
+
+        self.progress_bar.setValue(0)
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.val += 1
+        self.progress_bar.setValue(self.val/self.max * 100)
+
+        if self.val >= self.max:
+            raise StopIteration()
+
+        
+        return self.iter.__next__()
